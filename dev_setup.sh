b11cf149fdd0d907c27370d6db418965d01065c2
dev <mhuff17861>
2023-05-03 17:07:33 -0400

    moved things to functions in prod/dev_setup.sh, added ability to call install or update

--- dev_setup.sh
+++ dev_setup.sh
@@ -13,91 +13,101 @@ if [ $(id -u) != 0 ]; then
    exit
 fi
 
-# intial setup
+# Global Vars
 drupal_dir=drupal-src
 web_root=$drupal_dir/web
 apache_dir=setup/apache
 
-echo ""
-echo ""
-echo "Hey there! I need your username (non-root) for some crontab and composer installation shenanigans. If you would be so kind as to enter it below, that'd be great! Or you can watch the script crash and burn, that's cool too. I don't judge, fire is fun :)"
-read -p "Username: " uname
-echo ""
-
-apt update
+get_username () {
+    echo ""  >&2
+    echo ""  >&2
+    echo "Hey there! I need your username (non-root) for some crontab and composer installation shenanigans. If you would be so kind as to enter it below, that'd be great! Or you can watch the script crash and burn, that's cool too. I don't judge, fire is fun :)"  >&2
+    read -p "Username: " uname
+    echo ""  >&2
+    echo $uname
+}
 
 # Install and setup LAMP stack
-apt install lamp-server^
-apt install curl php8.1-xml php8.1-mbstring php8.1-gd php8.1-curl
-a2ensite default-ssl
-a2enmod ssl
-a2enmod rewrite
-a2enmod php8.1
+install_lamp_stack () {
+    uname=$1
+    apt update
+    apt install lamp-server^
+    apt install curl php8.1-xml php8.1-mbstring php8.1-gd php8.1-curl unzip
+    a2ensite default-ssl
+    a2enmod ssl
+    a2enmod rewrite
+    a2enmod php8.1
 
-mkdir $(pwd)/$web_root/sites/default/files
-mkdir /var/www
-rm -r /var/www/drupal
-mkdir $(pwd)/$web_root
-ln -s $(pwd)/$web_root /var/www/drupal
-chmod 775 /var/www
-chmod 775 /var/www/drupal
-chown -R $uname:www-data /var/www/drupal
-chown -R $uname:www-data $web_root
+    mkdir $(pwd)/$web_root/sites/default/files
+    mkdir /var/www
+    rm -r /var/www/drupal
+    mkdir $(pwd)/$web_root
+    ln -s $(pwd)/$web_root /var/www/drupal
+    chmod 775 /var/www
+    chmod 775 /var/www/drupal
+    chown -R $uname:www-data /var/www/drupal
+    chown -R $uname:www-data $web_root
 
-echo "" >> /etc/apache2/apache2.conf
-cat $(pwd)/$apache_dir/apache2-dev.conf >> /etc/apache2/apache2.conf
+    echo "" >> /etc/apache2/apache2.conf  >&2
+    cat $(pwd)/$apache_dir/apache2-dev.conf >> /etc/apache2/apache2.conf
 
-echo ""
-echo "You are about to be prompted to create an SSL cert for localhost. Enter localhost as the host name, enter IP:127.0.0.1 in alternative name(s) field."
-read -n1 -s -r -p $'Press any key to continue:' key
-echo ""
-make-ssl-cert /usr/share/ssl-cert/ssleay.cnf /etc/ssl/certs/localhost.crt
+    echo ""  >&2
+    echo "You are about to be prompted to create an SSL cert for localhost. Enter localhost as the host name, enter IP:127.0.0.1 in alternative name(s) field."  >&2
+    read -n1 -s -r -p $'Press any key to continue:' key
+    echo ""  >&2
+    make-ssl-cert /usr/share/ssl-cert/ssleay.cnf /etc/ssl/certs/localhost.crt
 
-rm /etc/apache2/sites-available/000-default.conf
-rm /etc/apache2/sites-available/default-ssl.conf
-ln -s $(pwd)/$apache_dir/dev-site.conf /etc/apache2/sites-available/000-default.conf
-ln -s $(pwd)/$apache_dir/ssl-dev.conf /etc/apache2/sites-available/default-ssl.conf
+    rm /etc/apache2/sites-available/000-default.conf
+    rm /etc/apache2/sites-available/default-ssl.conf
+    ln -s $(pwd)/$apache_dir/dev-site.conf /etc/apache2/sites-available/000-default.conf
+    ln -s $(pwd)/$apache_dir/ssl-dev.conf /etc/apache2/sites-available/default-ssl.conf
 
-service apache2 restart
-apt-get install php-ldap
-service apache2 restart
+    service apache2 restart
+    apt-get install php-ldap
+    service apache2 restart
 
-touch /home/$uname/autostart.sh
-printf '#!/bin/bash\nservice mysql start\nservice apache2 start\n' >> /home/$uname/autostart.sh
-chmod 777 /home/$uname/autostart.sh
-echo '# Allow apache2 and mysql to start without a password' >> /etc/sudoers
-echo '%sudo  ALL=(ALL) NOPASSWD: /usr/sbin/service apache2 *' >> /etc/sudoers
-echo '%sudo  ALL=(ALL) NOPASSWD: /usr/sbin/service mysql *' >> /etc/sudoers
+    touch /home/$uname/autostart.sh
+    printf '#!/bin/bash\nservice mysql start\nservice apache2 start\n' >> /home/$uname/autostart.sh
+    chmod 777 /home/$uname/autostart.sh
+    echo '# Allow apache2 and mysql to start without a password' >> /etc/sudoers
+    echo '%sudo  ALL=(ALL) NOPASSWD: /usr/sbin/service apache2 *' >> /etc/sudoers
+    echo '%sudo  ALL=(ALL) NOPASSWD: /usr/sbin/service mysql *' >> /etc/sudoers
+}
 
 # Composer Setup
+setup_composer () {
+    uname=$1
+    EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
 
-EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+    if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+    then
+        echo 'ERROR: Invalid installer checksum'  >&2
+        rm composer-setup.php
+        exit 1
+    fi
 
-if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
-then
-    >&2 echo 'ERROR: Invalid installer checksum'
+    php composer-setup.php
+    RESULT=$?
     rm composer-setup.php
-    exit 1
-fi
 
-php composer-setup.php
-RESULT=$?
-rm composer-setup.php
+    if [ $RESULT = 1 ]
+    then
+        echo "ERROR: Failed to install composer."  >&2
+        exit 1
+    fi
 
-if [ $RESULT = 1 ]
-then
-	echo "ERROR: Failed to install composer."
-	exit 1
-fi
+    sudo -u $uname php composer.phar install -d ./$drupal_dir
+}
 
-sudo -u $uname php composer.phar install -d ./$drupal_dir
-
-chown -R $uname:www-data $webroot
-chmod 770 drupal-src/web/sites/default/files
-chmod 750 drupal-src/web/modules
-chmod 640 drupal-src/web/sites/default/settings.php
+setup_permissions () {
+    uname=$1
+    chown -R $uname:www-data $webroot
+    chmod 770 drupal-src/web/sites/default/files
+    chmod 750 drupal-src/web/modules
+    chmod 640 drupal-src/web/sites/default/settings.php
+}
 
 # sudo chmod 640 drupal-src/web/sites/default/settings.php
 # Putting this here for as a reminder for the future production setup run
@@ -106,4 +116,27 @@ chmod 640 drupal-src/web/sites/default/settings.php
 
 # Install node package manager (npm) for scss stuff
 
-echo "Setup is almost complete! You should check the README for further instructions, including updating cron and installing fnm."
+initial_setup () {
+    uname=$(get_username)
+    install_lamp_stack $uname
+    setup_composer $uname
+    setup_permissions $uname
+    echo "Setup is almost complete! You should check the README for further instructions, including updating cron and installing fnm."  >&2
+}
+
+update_drupal() {
+    uname=$(get_username)
+    sudo -u $uname php composer.phar update "drupal/core-*" --with-all-dependencies -d ./$drupal_dir
+    echo "Go to [YOUR_DOMAIN]/update.php to complete updates." >&2
+    echo "NOTE: Sometimes it will tell you there are no updates, don't stress, you're fine, there are just no DB updates."  >&2
+}
+
+if [ $# -eq 0 ]; then
+    initial_setup
+elif [ "$1" == "--install" ]; then
+    initial_setup
+elif [ "$1" == "--update" ]; then
+    update_drupal
+else
+    echo "Improper argument(s) provided. Accepts --install or --update (defaults to --install with no args)"
+fi
--- prod_setup.sh
+++ prod_setup.sh
@@ -18,109 +18,151 @@ drupal_dir=drupal-src
 web_root=$drupal_dir/web
 apache_dir=setup/apache
 
-echo ""
-echo ""
-echo "Hey there! I need your username (non-root) for some crontab and composer installation shenanigans. If you would be so kind as to enter it below, that'd be great! Or you can watch the script crash and burn, that's cool too. I don't judge, fire is fun :)"
-read -p "Username: " uname
-echo ""
+get_username () {
+    echo ""  >&2
+    echo ""  >&2
+    echo "Hey there! I need your username (non-root) for some crontab and composer installation shenanigans. If you would be so kind as to enter it below, that'd be great! Or you can watch the script crash and burn, that's cool too. I don't judge, fire is fun :)"  >&2
+    read -p "Username: " uname
+    echo ""  >&2
+    echo $uname
+}
 
-echo ""
-echo ""
-echo "What is the domain you are building for, without www. (e.g. test.com)?"
-read -p "Domain: " domain
-echo ""
-
-apt update
+get_domain () {
+    echo "" >&2
+    echo "" >&2
+    echo "What is the domain you are building for, without www. (e.g. test.com)?" >&2
+    read -p "Domain: " domain
+    echo "" >&2
+}
 
 # Install and setup LAMP stack
-apt install -y lamp-server^
-apt install -y curl php8.1-xml php8.1-mbstring php8.1-gd php8.1-curl
-a2enmod rewrite
-a2enmod php8.1
+install_lamp_stack () {
+    uname=$1
+    domain=$2
+    apt update
 
-# Install Certbot
-apt install -y python3 unzip
-apt install -y certbot python3-certbot-apache
+    apt install -y lamp-server^
+    apt install -y curl php8.1-xml php8.1-mbstring php8.1-gd php8.1-curl
+    a2enmod rewrite
+    a2enmod php8.1
 
-# Folder Structure Setup
-mkdir /var/www
-rm -r /var/www/drupal
-mkdir $(pwd)/$web_root
-ln -s $(pwd)/$web_root /var/www/drupal
-chmod 775 /var/www
-chmod 775 /var/www/drupal
-chown -R $uname:www-data /var/www/drupal
-chown -R $uname:www-data $web_root
+    # Install Certbot
+    apt install -y python3 unzip
+    apt install -y certbot python3-certbot-apache
 
-# Apache configuration
-echo "" >> /etc/apache2/apache2.conf
-cp $(pwd)/$apache_dir/apache2-prod.conf $(pwd)/apache2-prod.conf
-sed -i -e "s/{{domain}}/$domain/g" $(pwd)/apache2-prod.conf
-cat $(pwd)/apache2-prod.conf >> /etc/apache2/apache2.conf
-rm $(pwd)/apache2-prod.conf
+    # Folder Structure Setup
+    mkdir /var/www
+    rm -r /var/www/drupal
+    mkdir $(pwd)/$web_root
+    ln -s $(pwd)/$web_root /var/www/drupal
+    chmod 775 /var/www
+    chmod 775 /var/www/drupal
+    chown -R $uname:www-data /var/www/drupal
+    chown -R $uname:www-data $web_root
 
-cp /etc/apache2/sites-available/$domain.conf
-sed -i -e "s/{{domain}}/$domain/g" /etc/apache2/sites-available/$domain.conf
+    # Apache configuration
+    echo "" >> /etc/apache2/apache2.conf
+    cp $(pwd)/$apache_dir/apache2-prod.conf $(pwd)/apache2-prod.conf
+    sed -i -e "s/{{domain}}/$domain/g" $(pwd)/apache2-prod.conf
+    cat $(pwd)/apache2-prod.conf >> /etc/apache2/apache2.conf
+    rm $(pwd)/apache2-prod.conf
 
-service httpd restart
-service apache2 restart
-apt-get install -y php-ldap
-service apache2 restart
+    cp /etc/apache2/sites-available/$domain.conf
+    sed -i -e "s/{{domain}}/$domain/g" /etc/apache2/sites-available/$domain.conf
+
+    service httpd restart
+    service apache2 restart
+    apt-get install -y php-ldap
+    service apache2 restart
+
+    setup_https
+
+    # sudoers config for auto start
+    touch /home/$uname/autostart.sh
+    printf '#!/bin/bash\nservice mysql start\nservice apache2 start\n' >> /home/$uname/autostart.sh
+    chmod 777 /home/$uname/autostart.sh
+    echo '# Allow apache2 and mysql to start without a password' >> /etc/sudoers
+    echo '%sudo  ALL=(ALL) NOPASSWD: /usr/sbin/service apache2 *' >> /etc/sudoers
+    echo '%sudo  ALL=(ALL) NOPASSWD: /usr/sbin/service mysql *' >> /etc/sudoers
+}
 
 # HTTPS setup with certbot
-ufw allow 'Apache Full'
-ufw delete allow 'Apache'
-echo ""
-echo "Printing firewall status:\n"
-echo ""
-ufw status
+setup_https () {
+    uname=$1
+    ufw allow 'Apache Full'
+    ufw delete allow 'Apache'
+    echo ""  >&2
+    echo "Printing firewall status:\n"  >&2
+    echo ""  >&2
+    ufw status
 
-echo ""
-echo "You are about to be prompted by certbot in this next section to setup HTTPS. Just a quick pause/alertness check before starting."
-read -n1 -s -r -p $'Press any key to continue:' key
-echo ""
+    echo ""  >&2
+    echo "You are about to be prompted by certbot in this next section to setup HTTPS. Just a quick pause/alertness check before starting."  >&2
+    read -n1 -s -r -p $'Press any key to continue:' key
+    echo ""  >&2
 
-certbot --apache
-
-# sudoers config for auto start
-touch /home/$uname/autostart.sh
-printf '#!/bin/bash\nservice mysql start\nservice apache2 start\n' >> /home/$uname/autostart.sh
-chmod 777 /home/$uname/autostart.sh
-echo '# Allow apache2 and mysql to start without a password' >> /etc/sudoers
-echo '%sudo  ALL=(ALL) NOPASSWD: /usr/sbin/service apache2 *' >> /etc/sudoers
-echo '%sudo  ALL=(ALL) NOPASSWD: /usr/sbin/service mysql *' >> /etc/sudoers
+    certbot --apache
+}
 
 # Composer Setup
-EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+setup_composer () {
+    uname=$1
+    EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
 
-if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
-then
-    >&2 echo 'ERROR: Invalid installer checksum'
+    if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+    then
+        echo 'ERROR: Invalid installer checksum' >&2
+        rm composer-setup.php
+        exit 1
+    fi
+
+    php composer-setup.php
+    RESULT=$?
     rm composer-setup.php
-    exit 1
+
+    if [ $RESULT = 1 ]
+    then
+        echo "ERROR: Failed to install composer."  >&2
+        exit 1
+    fi
+
+    sudo -u $uname php composer.phar install -d ./$drupal_dir
+}
+
+setup_permissions () {
+    uname=$1
+    chown -R $uname:www-data $webroot
+    chmod -R 770 drupal-src/web/sites/default/files
+    chmod -R 750 drupal-src/web/modules
+    chmod 750 drupal-src/web/index.php
+    chmod 640 *.php
+    chmod 440 drupal-src/web/sites/default/settings.php
+}
+
+initial_setup () {
+    uname=$(get_username)
+    domain=$(get_domain)
+    install_lamp_stack $uname $domain
+    setup_composer $uname
+    setup_permissions $uname
+    echo "Setup is almost complete! You should check the README for further instructions, including updating cron and installing fnm."  >&2
+}
+
+update_drupal() {
+    uname=$(get_username)
+    sudo -u $uname php composer.phar install --no-dev -d ./$drupal_dir
+    echo "Go to [YOUR_DOMAIN]/update.php to complete updates." >&2
+    echo "NOTE: Sometimes it will tell you there are no updates, don't stress, you're fine, there are just no DB updates."  >&2
+}
+
+if [ $# -eq 0 ]; then
+    initial_setup
+elif [ "$1" == "--install" ]; then
+    initial_setup
+elif [ "$1" == "--update" ]; then
+    update_drupal
+else
+    echo "Improper argument(s) provided. Accepts --install or --update (defaults to --install with no args)"
 fi
-
-php composer-setup.php
-RESULT=$?
-rm composer-setup.php
-
-if [ $RESULT = 1 ]
-then
-	echo "ERROR: Failed to install composer."
-	exit 1
-fi
-
-sudo -u $uname php composer.phar install -d ./$drupal_dir
-
-chown -R $uname:www-data $webroot
-chmod -R 770 drupal-src/web/sites/default/files
-chmod -R 750 drupal-src/web/modules
-chmod 750 drupal-src/web/index.php
-chmod 640 *.php
-chmod 440 drupal-src/web/sites/default/settings.php
-
-# Install node package manager (npm) for scss stuff
-
-echo "Setup is almost complete! You should check the README for further instructions, including updating cron and installing fnm."
